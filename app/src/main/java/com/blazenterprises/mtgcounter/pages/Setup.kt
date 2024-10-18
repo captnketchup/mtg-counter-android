a583c741f438b95ee47bf45696cacfb7ea069ce7
Daniel Kovari <74988054+captnketchup@users.noreply.github.com>
2024-10-18 14:49:42 +0200

    feat: add navigation and menu to starting screen

---
+++ app/src/main/java/com/blazenterprises/mtgcounter/pages/Setup.kt
@@ -0,0 +1,2 @@
+package com.blazenterprises.mtgcounter.Pages
+
---
+++ app/src/main/java/com/blazenterprises/mtgcounter/pages/configuration/LifeSetup.kt
@@ -0,0 +1,25 @@
+package com.blazenterprises.mtgcounter.pages.configurationPages
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun StartGameScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Start Game Screen")
+        // Add other UI elements as needed
+    }
+}
